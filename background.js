305cd3aae61a181ffa06e5d56edf12faf5158b1f
kami <kami@tumult.MA>
2021-01-12 16:57:52 +0000

    initial import

---
+++ background.js
@@ -0,0 +1,16 @@
+// create a context menu
+browser.contextMenus.create({
+	id: "GitSearch",
+	title: "Search on GitHub",
+	contexts: ["selection"]
+});
+
+// add action listener to the context menu
+
+browser.contextMenus.onClicked.addListener(launchMyGitSearch);
+
+function launchMyGitSearch(info, tab){
+	const url = "https://github.com/search?q=" + info.selectionText;
+
+	  browser.tabs.create({url:url});
+}
